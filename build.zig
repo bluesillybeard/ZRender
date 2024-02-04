9ce6513db6e4ffd01f3234fe0ff6816324a0fad7
bluesillybeard <bluesillybeard2@gmail.com>
2024-02-04 16:15:14 -0700

    bird

--- build.zig
+++ build.zig
@@ -2,20 +2,24 @@ const std = @import("std");
 const builtin = @import("builtin");
 const kinc = @import("Kinc.zig/build.zig");
 
-pub fn link(comptime modulePath: []const u8, c: *std.Build.Step.Compile) !void {
+pub fn link(comptime modulePath: []const u8, c: *std.Build.Step.Compile, zengine: *std.Build.Module) !void {
     // TODO: take in options as parameters
     const options = kinc.KmakeOptions{
         .platform = .guess,
     };
     const realModulePath = modulePath ++ "/Kinc.zig/Kinc";
-    try kinc.link(realModulePath, c, options);
-    try kinc.compileShader(realModulePath, c, modulePath ++ "/src/shaders/shader.frag.glsl", modulePath ++ "src/shaderBin/shader.frag", options);
-    try kinc.compileShader(realModulePath, c, modulePath ++ "/src/shaders/shader.vert.glsl", modulePath ++ "src/shaderBin/shader.vert", options);
-
-    const zrender = c.root_module.owner.createModule(.{
+    const zrender = c.root_module.owner.addStaticLibrary(.{
+        .name = "zrender",
         .root_source_file = .{.path = modulePath ++ "/src/zrender.zig"},
-        .target = c.root_module.resolved_target,
-        .optimize = c.root_module.optimize,
+        .target = c.root_module.resolved_target.?,
+        .optimize = c.root_module.optimize.?,
     });
-    c.root_module.addImport("zrender", zrender);
+    zrender.root_module.addImport("zengine", zengine);
+
+    try kinc.link(realModulePath, zrender, options);
+    try kinc.compileShader(realModulePath, zrender, modulePath ++ "/src/shaders/shader.frag.glsl", modulePath ++ "/src/shaderBin/shader.frag", options);
+    try kinc.compileShader(realModulePath, zrender, modulePath ++ "/src/shaders/shader.vert.glsl", modulePath ++ "/src/shaderBin/shader.vert", options);
+
+    
+    c.root_module.addImport("zrender", &zrender.root_module);
 }
--- src/zrender.zig
+++ src/zrender.zig
@@ -1,7 +1,16 @@
 const std = @import("std");
-const registry = @import("registry.zig");
-const zengine = @import("zengine.zig");
+const zengine = @import("zengine");
 const ecs = @import("ecs");
+
+const c = @cImport({
+    @cInclude("kinc/graphics4/graphics.h");
+    @cInclude("kinc/graphics4/indexbuffer.h");
+    @cInclude("kinc/graphics4/pipeline.h");
+    @cInclude("kinc/graphics4/shader.h");
+    @cInclude("kinc/graphics4/vertexbuffer.h");
+    @cInclude("kinc/graphics4/texture.h");
+    @cInclude("kinc/system.h");
+});
 
 pub const RenderComponent = struct {
     mesh: Mesh,
@@ -18,10 +27,14 @@ pub const Vertex = extern struct {
     blend: f32,
 };
 
-// A handle to a mesh object
-pub const Mesh = usize;
+pub const Mesh = struct {
+    c.kinc_g4_index_buffer,
+    c.kinc_g4_vertex_buffer,
+};
 
-pub const Texture = usize;
+pub const Texture = struct {
+    //TODO
+};
 
 pub const Mat4 = extern struct {
     m00: f32, m01: f32, m02: f32, m03: f32,
@@ -48,24 +61,139 @@ pub const ZRenderSystem = struct {
     }
 
     pub fn init(staticAllocator: std.mem.Allocator, heapAllocator: std.mem.Allocator) @This() {
-        _ = heapAllocator;
         _ = staticAllocator;
-        return .{};
+        return ZRenderSystem{
+            .pipeline = .{},
+            .vertices = .{},
+            .indices = .{},
+            .texture = .{},
+            .textureUnit = .{},
+            .transformLocation = .{},
+            .allocator = heapAllocator,
+        };
     }
 
-    pub fn systemInitGlobal(this: *@This(), registries: zengine.RegistrySet) !void {
+    pub fn systemInitGlobal(this: *@This(), registries: *zengine.RegistrySet) !void {
         _ = registries;
         _ = this;
     }
 
-    pub fn systemDeinitGlobal(this: *@This(), registries: zengine.RegistrySet) void {
+    pub fn systemDeinitGlobal(this: *@This(), registries: *zengine.RegistrySet) void {
         _ = registries;
         _ = this;
     }
 
     pub fn deinit(this: *@This()) void {
         _ = this;
-
     }
+
+    pub fn initZRender(this: *@This(), registries: *zengine.RegistrySet) !void {
+        var vertex_shader: c.kinc_g4_shader_t = .{};
+        var fragment_shader: c.kinc_g4_shader_t = .{};
+        _ = c.kinc_init("Shader", 1024, 768, null, null);
+        c.kinc_set_update_callback(&update, registries);
+        // In the original test, the shaders are loaded in a separate function
+        // and the "allocator" is a buffer allocator to a chunk of memory.
+        // Instead, the @embedFile builtin is used, which eliminates the need to allocate memory or load a file at runtime.
+        const vertexShaderCode = @embedFile("shaderBin/shader.vert");
+        c.kinc_g4_shader_init(&vertex_shader, vertexShaderCode.ptr, vertexShaderCode.len, c.KINC_G4_SHADER_TYPE_VERTEX);
+        const fragmentShaderCode = @embedFile("shaderBin/shader.frag");
+        c.kinc_g4_shader_init(&fragment_shader, fragmentShaderCode.ptr, fragmentShaderCode.len, c.KINC_G4_SHADER_TYPE_FRAGMENT);
+
+
+        var structure: c.kinc_g4_vertex_structure_t = .{};
+        c.kinc_g4_vertex_structure_init(&structure);
+        c.kinc_g4_vertex_structure_add(&structure, "pos", c.KINC_G4_VERTEX_DATA_F32_3X);
+        c.kinc_g4_vertex_structure_add(&structure, "texCoord", c.KINC_G4_VERTEX_DATA_F32_2X);
+        c.kinc_g4_vertex_structure_add(&structure, "color", c.KINC_G4_VERTEX_DATA_U8_4X_NORMALIZED);
+        c.kinc_g4_vertex_structure_add(&structure, "blend", c.KINC_G4_VERTEX_DATA_F32_1X);
+        c.kinc_g4_pipeline_init(&this.pipeline);
+        this.pipeline.vertex_shader = &vertex_shader;
+        this.pipeline.fragment_shader = &fragment_shader;
+        this.pipeline.input_layout[0] = &structure;
+        this.pipeline.input_layout[1] = null;
+        c.kinc_g4_pipeline_compile(&this.pipeline);
+        c.kinc_g4_vertex_buffer_init(&this.vertices, 3, &structure, c.KINC_G4_USAGE_STATIC, 0);
+        {
+            const v: *[3]Vertex = @ptrCast(c.kinc_g4_vertex_buffer_lock_all(&this.vertices));
+            v.* = [3]Vertex {
+                Vertex{.x = -1, .y = -1, .z = 0.5, .texX = 0, .texY = 0, .color = 0xFFFFFF00, .blend = 1},
+                Vertex{.x =  1, .y = -1, .z = 0.5, .texX = 1, .texY = 0, .color = 0xFF00FFFF, .blend = 1},
+                Vertex{.x = -1, .y =  1, .z = 0.5, .texX = 0, .texY = 1, .color = 0xFFFF00FF, .blend = 0},
+            };
+            c.kinc_g4_vertex_buffer_unlock_all(&this.vertices);
+        }
+        c.kinc_g4_index_buffer_init(&this.indices, 3, c.KINC_G4_INDEX_BUFFER_FORMAT_16BIT, c.KINC_G4_USAGE_STATIC);
+        {
+            const i: *[3]u16 = @alignCast(@ptrCast(c.kinc_g4_index_buffer_lock_all(&this.indices)));
+            i.* = [3]u16{0, 1, 2};
+            c.kinc_g4_index_buffer_unlock_all(&this.indices);
+        }
+        var image = c.kinc_image{};
+        const memory: []u8 = try this.allocator.alloc(u8, 250 * 250 * 4);
+        const data = @embedFile("parrot.png");
+        //_ = c.kinc_image_init_from_file(&image, @ptrCast(&data), "parrot.png");
+        _ = c.kinc_image_init_from_encoded_bytes(&image, memory.ptr, @constCast(@ptrCast(data.ptr)), data.len, "png");
+        c.kinc_g4_texture_init_from_image(&this.texture, &image);
+        this.textureUnit = c.kinc_g4_pipeline_get_texture_unit(&this.pipeline, "tex");
+        this.transformLocation = c.kinc_g4_pipeline_get_constant_location(&this.pipeline, "transform");
+    }
+
+    pub fn run(this: *@This()) void {
+        _ = this;
+        c.kinc_start();
+    }
+
+    fn mat4ToKinc(matrix: Mat4) c.kinc_matrix4x4 {
+        // TODO: see if this can be done more optimally
+        var m = c.kinc_matrix4x4{};
+        c.kinc_matrix4x4_set(&m, 0, 0, matrix.m00);
+        c.kinc_matrix4x4_set(&m, 0, 1, matrix.m01);
+        c.kinc_matrix4x4_set(&m, 0, 2, matrix.m02);
+        c.kinc_matrix4x4_set(&m, 0, 3, matrix.m03);
+
+        c.kinc_matrix4x4_set(&m, 1, 0, matrix.m10);
+        c.kinc_matrix4x4_set(&m, 1, 1, matrix.m11);
+        c.kinc_matrix4x4_set(&m, 1, 2, matrix.m12);
+        c.kinc_matrix4x4_set(&m, 1, 3, matrix.m13);
+
+        c.kinc_matrix4x4_set(&m, 2, 0, matrix.m20);
+        c.kinc_matrix4x4_set(&m, 2, 1, matrix.m21);
+        c.kinc_matrix4x4_set(&m, 2, 2, matrix.m22);
+        c.kinc_matrix4x4_set(&m, 2, 3, matrix.m23);
+
+        c.kinc_matrix4x4_set(&m, 3, 0, matrix.m30);
+        c.kinc_matrix4x4_set(&m, 3, 1, matrix.m31);
+        c.kinc_matrix4x4_set(&m, 3, 2, matrix.m32);
+        c.kinc_matrix4x4_set(&m, 3, 3, matrix.m33);
+        return m;
+    }
+
+    fn update(_data: ?*anyopaque) callconv(.C) void {
+        // cast the opaque pointer to what it was previously
+        const registries: *zengine.RegistrySet = @alignCast(@ptrCast(_data));
+        // get this
+        var this = registries.globalRegistry.getRegister(ZRenderSystem).?;
+
+        c.kinc_g4_begin(0);
+        c.kinc_g4_clear(c.KINC_G4_CLEAR_COLOR, 0, 0.0, 0);
+        c.kinc_g4_set_pipeline(&this.pipeline);
+        var identity = mat4ToKinc(Mat4.identity);
+        c.kinc_g4_set_matrix4(this.transformLocation, &identity);
+        c.kinc_g4_set_texture(this.textureUnit, &this.texture);
+        c.kinc_g4_set_vertex_buffer(&this.vertices);
+        c.kinc_g4_set_index_buffer(&this.indices);
+        c.kinc_g4_set_texture(this.textureUnit, &this.texture);
+        c.kinc_g4_draw_indexed_vertices();
+        c.kinc_g4_end(0);
+        _ = c.kinc_g4_swap_buffers();
+    }
+    pipeline: c.kinc_g4_pipeline,
+    vertices: c.kinc_g4_vertex_buffer,
+    indices: c.kinc_g4_index_buffer,
+    texture: c.kinc_g4_texture,
+    textureUnit: c.kinc_g4_texture_unit,
+    transformLocation: c.kinc_g4_constant_location,
+    allocator: std.mem.Allocator,
 };
 
