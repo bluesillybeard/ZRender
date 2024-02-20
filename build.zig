aae79fc82d72084fa130ece0406a8f2944e50e52
bluesillybeard <bluesillybeard2@gmail.com>
2024-02-19 18:06:01 -0700

    improve redability of build.zig

--- build.zig
+++ build.zig
@@ -7,20 +7,19 @@ pub fn link(comptime modulePath: []const u8, c: *std.Build.Step.Compile, zengine
     const options = kinc.KmakeOptions{
         .platform = .guess,
     };
-    const realModulePath = modulePath ++ "/Kinc.zig/Kinc";
+    const kincModulePath = modulePath ++ "/Kinc.zig/Kinc";
     const zrender = c.root_module.owner.addStaticLibrary(.{
         .name = "zrender",
-        .root_source_file = .{.path = modulePath ++ "/src/zrender.zig"},
+        .root_source_file = .{ .path = modulePath ++ "/src/zrender.zig" },
         .target = c.root_module.resolved_target.?,
         .optimize = c.root_module.optimize.?,
     });
     zrender.root_module.addImport("zengine", zengine);
     zrender.root_module.addImport("ecs", ecs);
 
-    try kinc.link(realModulePath, zrender, options);
-    try kinc.compileShader(realModulePath, zrender, modulePath ++ "/src/shaders/shader.frag.glsl", modulePath ++ "/src/shaderBin/shader.frag", options);
-    try kinc.compileShader(realModulePath, zrender, modulePath ++ "/src/shaders/shader.vert.glsl", modulePath ++ "/src/shaderBin/shader.vert", options);
+    try kinc.link(kincModulePath, zrender, options);
+    try kinc.compileShader(kincModulePath, zrender, modulePath ++ "/src/shaders/shader.frag.glsl", modulePath ++ "/src/shaderBin/shader.frag", options);
+    try kinc.compileShader(kincModulePath, zrender, modulePath ++ "/src/shaders/shader.vert.glsl", modulePath ++ "/src/shaderBin/shader.vert", options);
 
-    
     c.root_module.addImport("zrender", &zrender.root_module);
 }
